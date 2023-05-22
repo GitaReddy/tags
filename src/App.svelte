f1c1a79a2a65d00b486fa8c69885cc986506386f
geethasree.puli <geethasree.puli@gmail.com>
2023-05-22 17:03:56 +0530

    tagify_tags

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+import Tags from 'svelte-tags-input'
+	
+// If on:tags is defined
+let tag = "";
+
+function handleTags(event) {
+    tag = event.detail.tags;
+}
+
+
+</script>
+
+<Tags
+    on:tags={handleTags}
+    addKeys={[9]}
+    maxTags={5}
+    allowPaste={true}
+    allowDrop={true}
+    splitWith={"/"}
+    onlyUnique={false}
+    removeKeys={[27]}
+    placeholder={"Type Input "}
+    
+    name={"custom-name"}
+    id={"custom-id"}
+    allowBlur={true}
+    disable={false}
+    minChars={3}
+		
+/>
+
